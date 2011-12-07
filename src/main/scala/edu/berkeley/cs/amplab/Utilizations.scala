408498f29f39dbf736075ffda7f8e3fd2fdeb5d1
Charles Reiss <charles@eecs.berkeley.edu>
2011-12-07 00:38:47 -0800

    More Utilization collection stuff.

--- src/main/scala/edu/berkeley/cs/amplab/Utilizations.scala
+++ src/main/scala/edu/berkeley/cs/amplab/Utilizations.scala
@@ -25,27 +25,32 @@ import com.twitter.elephantbird.mapreduce.io.ProtobufWritable
 import com.google.protobuf.Message
 
 object Utilizations {
+  // TODO(Charles Reiss): weight by length of measurements.
   def getPercentile(p: Double, s: IndexedSeq[Float]): Float = {
     val place = ((s.size - 1) * p)
-    val low = s(place.floor)
-    val high = s(place.ceil)
+    val low = s(place.floor.asInstanceOf[Int])
+    val high = s(place.ceil.asInstanceOf[Int])
     val offset = place - place.floor
     return (low * (1.0 - offset) + high * offset).asInstanceOf[Float]
   }
 
-  val PER_TASK_PERCENTILES = List(0.5, 0.9, 0.95, 0.99, 1.0)
+  val PER_TASK_PERCENTILES = List(0.5, 0.9, 0.99, 1.0)
   val JOB_PERCENTILES = List(0.5, 0.99, 1.0)
 
   def getTaskUtilization(usage: Seq[TaskUsage]): Option[TaskUtilization] = {
     val result = TaskUtilization.newBuilder
     val firstUsage = usage.head
-    result.setTaskInfo(firstUsage.getTaskInfo)
+    result.setInfo(firstUsage.getTaskInfo)
 
     val taskInfos =
       usage.filter(u => u.hasTaskInfo && u.getTaskInfo.hasRequestedResources).
             map(_.getTaskInfo)
     if (taskInfos.size == 0)
       return None
+
+    result.setStartTime(usage.map(_.getStartTime).min).
+           setEndTime(usage.map(_.getEndTime).max).
+           setRunningTime(usage.map(u => u.getEndTime - u.getStartTime).sum)
 
     val minReqBuilder = result.getMinRequestBuilder
     val maxReqBuilder = result.getMaxRequestBuilder
@@ -55,10 +60,10 @@ object Utilizations {
     minReqBuilder.setMemory(taskInfos.map(_.getRequestedResources.getMemory).min)
     maxReqBuilder.setMemory(taskInfos.map(_.getRequestedResources.getMemory).max)
 
-    val maxCpus = usage.map(_.getMaxResources.getCpus).sorted
-    val maxMemory = usage.map(_.getMaxResources.getMemory).sorted
-    val cpus = usage.map(_.getResources.getCpus).sorted
-    val memory = usage.map(_.getResources.getMemory).sorted
+    val maxCpus = usage.map(_.getMaxResources.getCpus).sorted.toIndexedSeq
+    val maxMemory = usage.map(_.getMaxResources.getMemory).sorted.toIndexedSeq
+    val cpus = usage.map(_.getResources.getCpus).sorted.toIndexedSeq
+    val memory = usage.map(_.getResources.getMemory).sorted.toIndexedSeq
 
     for (percentile <- PER_TASK_PERCENTILES) {
       val meanUsage = Resources.newBuilder.
@@ -78,32 +83,39 @@ object Utilizations {
   }
 
   def getJobUtilization(tasks: Seq[TaskUtilization]): JobUtilization = {
+    import scala.collection.JavaConversions._
     val result = JobUtilization.newBuilder
-    val firstTask = tasks.first
+    val firstTask = tasks.head
     result.setJobInfo(firstTask.getInfo.getJob)
+    val numTasks = tasks.groupBy(_.getInfo.getTaskIndex).size
+    result.setNumTasks(numTasks)
 
     val minReqBuilder = result.getMinRequestBuilder
     val maxReqBuilder = result.getMaxRequestBuilder
     minReqBuilder.setCpus(tasks.map(_.getMinRequest.getCpus).min).
-                 .setMemory(tasks.map(_.getMinRequest.getMemory).min)
+                  setMemory(tasks.map(_.getMinRequest.getMemory).min)
     maxReqBuilder.setCpus(tasks.map(_.getMaxRequest.getCpus).max).
-                 .setMemory(tasks.map(_.getMaxRequest.getMemory).max)
+                  setMemory(tasks.map(_.getMaxRequest.getMemory).max)
 
-    for ((p, i) <- PER_TASK_PERCENTILES.zipWithIndex) {
-      val maxCpus = tasks.map(_.getPercentileTaskUsage(i).getCpus).sorted
-      val maxMemory = tasks.map(_.getPercentileTaskUsage(i).getMemory).sorted
-      val cpus = tasks.map(_.getPercentileMeanTaskUsage(i).getCpus).sorted
-      val memory = tasks.map(_.getPercentileMeanTaskUsage(i).getCpus).sorted
+    val percentiles = firstTask.getUsagePercentileList
+    for ((p, i) <- percentiles.zipWithIndex) {
+      val maxCpus = tasks.map(_.getPercentileTaskUsage(i).getCpus).sorted.toIndexedSeq
+      val maxMemory = tasks.map(_.getPercentileTaskUsage(i).getMemory).sorted.toIndexedSeq
+      val cpus = tasks.map(_.getPercentileMeanTaskUsage(i).getCpus).sorted.toIndexedSeq
+      val memory = tasks.map(_.getPercentileMeanTaskUsage(i).getMemory).sorted.toIndexedSeq
 
       for (p2 <- JOB_PERCENTILES) {
-        result.addTaskPercentile(p2).addUsagePercentile(p).
+        result.addTaskPercentile(p2.asInstanceOf[Float]).
+               addUsagePercentile(p.asInstanceOf[Float]).
           addPercentileTaskUsage(Resources.newBuilder.
-            setCpus(percentile(p2, maxCpus)).
-            setMemory(percentile(p2, maxMemory))).
+            setCpus(getPercentile(p2, maxCpus)).
+            setMemory(getPercentile(p2, maxMemory))).
           addPercentileMeanTaskUsage(Resources.newBuilder.
-            setCpus(percentile(p2, cpus)).
-            setMemory(percentile(p2, memory)))
+            setCpus(getPercentile(p2, cpus)).
+            setMemory(getPercentile(p2, memory)))
       }
     }
+
+    result.build
   }
 }
